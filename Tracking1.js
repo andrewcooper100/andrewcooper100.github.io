e0c1628f783813c35e708e3c4dc8dc2b39975318
andrewcooper100 <51785050+andrewcooper100@users.noreply.github.com>
2019-06-18 10:36:11 +0100

    Update Tracking1.js

--- Tracking1.js
+++ Tracking1.js
@@ -1,24 +1,12 @@
-!-- Global site tag (gtag.js) - Google Ads: 851231605 --
 async src="https://www.googletagmanager.com/gtag/js?id=AW-851231605"
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'AW-851231605');
-!-- Event snippet for Step 1 Complete Lead conversion page --
   gtag('event', 'conversion', {'send_to': 'AW-851231605/bR7RCK__o6IBEPWG85UD'});
 
 
-!function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1761999997420197');
-  fbq('track', 'PageView');
-  fbq('track', 'Lead');
+
 
 
 
