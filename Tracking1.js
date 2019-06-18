750580eecdbc7bf76f8b9455a672deff78fd913c
andrewcooper100 <51785050+andrewcooper100@users.noreply.github.com>
2019-06-18 10:24:52 +0100

    Update Tracking1.js

--- Tracking1.js
+++ Tracking1.js
@@ -11,5 +11,11 @@
   fbq('track', 'Lead');
 
 
+async src="https://www.googletagmanager.com/gtag/js?id=AW-851231605"
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-851231605');
 
 gtag('event', 'conversion', {'send_to': 'AW-851231605/bR7RCK__o6IBEPWG85UD'});
