133f2da0ae3beac86e05b6dabe7c6b9a38724a7d
andrewcooper100 <51785050+andrewcooper100@users.noreply.github.com>
2019-06-18 10:33:16 +0100

    Update Tracking1.js

--- Tracking1.js
+++ Tracking1.js
@@ -1,3 +1,13 @@
+!-- Global site tag (gtag.js) - Google Ads: 851231605 --
+async src="https://www.googletagmanager.com/gtag/js?id=AW-851231605"
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-851231605');
+!-- Event snippet for Step 1 Complete Lead conversion page --
+  gtag('event', 'conversion', {'send_to': 'AW-851231605/bR7RCK__o6IBEPWG85UD'});
+
+
 !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -12,4 +22,3 @@
 
 
 
-gtag('event', 'conversion', {'send_to': 'AW-851231605/bR7RCK__o6IBEPWG85UD'});
